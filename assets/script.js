99d2335f82ee641fd7ec238faaf01042a78b2673
hassanmahdi58 <hassanmahdi58@gmail.com>
2022-01-29 19:26:02 +0000

    main

--- assets/script.js
+++ assets/script.js
@@ -39,28 +39,26 @@ function currentWeather(city){
         method:"GET",
     }).then(function(response){
 
-        // parse the response to display the current weather including the City name. the Date and the weather icon. 
+      
         console.log(response);
-        //Dta object from server side Api for icon property.
+     
         var weathericon= response.weather[0].icon;
         var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
-        // The date format method is taken from the  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+      
         var date=new Date(response.dt*1000).toLocaleDateString();
-        //parse the response for name of city and concanatig the date and icon.
+      
         $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
-        // parse the response to display the current temperature.
-        // Convert the temp to fahrenheit
+       
 
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
         $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
-        // Display the Humidity
+       
         $(currentHumidty).html(response.main.humidity+"%");
         //Display Wind speed and convert to MPH
         var ws=response.wind.speed;
         var windsmph=(ws*2.237).toFixed(1);
         $(currentWSpeed).html(windsmph+"MPH");
-        // Display UVIndex.
-        //By Geographic coordinates method and using appid and coordinates as a parameter we are going build our uv query url inside the function below.
+      
         UVIndex(response.coord.lon,response.coord.lat);
         forecast(response.id);
         if(response.cod==200){
@@ -105,18 +103,23 @@ function forecast(cityid){
         method:"GET"
     }).then(function(response){
         
-        for (i=0;i<5;i++){
+        for (i=0;i<5;i++
+            ){
             var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
             var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
             var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
             var tempK= response.list[((i+1)*8)-1].main.temp;
             var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
             var humidity= response.list[((i+1)*8)-1].main.humidity;
+            var humidity= response.list[((i+1)*6)-1].main.humidity;
+           
         
             $("#fDate"+i).html(date);
             $("#fImg"+i).html("<img src="+iconurl+">");
             $("#fTemp"+i).html(tempF+"&#8457");
             $("#fHumidity"+i).html(humidity+"%");
+            $("#fHumidity"+i).html(humidity+"%");
+            
         }
         
     });
@@ -161,7 +164,7 @@ function clearHistory(event){
     document.location.reload();
 
 }
-//Click Handlers
+
 $("#search-button").on("click",displayWeather);
 $(document).on("click",invokePastSearch);
 $(window).on("load",loadlastCity);
